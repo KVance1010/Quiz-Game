ddc018ddfbf00f35c2fd9f8cef412619c0102e28
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-09-30 08:12:54 -0700

    pseudocode and started project created

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// declare variables for name, correctScore, wrongScore, firstPlace, secondPlace, thirdPlace
+
+
+// create an array of objects that holds question and answer objects
+
+// create a timer
+// create a function that randomly picks a question
+// create a variables that holds the usersAnswer
+// create an event listener that will hold usersAnswer
+// check if the usersAnswer matches the correctAnswer
+// if the userAnswer matches the answer then increment the correctScore counter and display a correct message
+// if the userAnswer does not matches the answer then increment the wrongScore counter, display the correct answer, and decrees the counter clock by 10 seconds
+// have the correctScore, wrongScore, and counter clock displayed at the top of the screen
+// at the end of the quiz display the correctScore, wrongScore
+// if the correctScore is in the top 3 scores get the users initials and place them on the leaderboard
+// else tell them they did not make the top 3 scores
